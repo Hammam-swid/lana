a7e0f16804f7f2f33818d4a5b77455fb115fbe6c
Hammam Swaid <hmam.swid@gmail.com>
2024-02-22 16:22:01 +0200

    adding non-complete createPost function

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,4 +1,7 @@
 const Post = require("../models/postModel");
+const catchAsync = require("../utils/catchAsync");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+// const dotenv = require()
 
 exports.getPosts = async (req, res, next) => {
   try {
@@ -16,3 +19,22 @@ exports.getPosts = async (req, res, next) => {
     });
   }
 };
+
+exports.createPost = catchAsync(async (req, res, next) => {
+  const { content, photos } = req.body;
+  if (content) {
+    const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+    const textModel = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const visionModel = genAI.getGenerativeModel({
+      model: "gemini-pro-vision",
+    });
+    const prompt = `what are the categories for this content: '${content}'`;
+    const result = await textModel.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+    res.status(201).json({
+      text,
+    });
+  }
+});
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,13 +1,12 @@
-const express = require( "express");
-const { getPosts } = require("../controllers/postController");
+const express = require("express");
+const { getPosts, createPost } = require("../controllers/postController");
 const { protect } = require("../controllers/authController");
-
 
 // إنشاء موجه فرعي خاص بالمناشير
 const router = express.Router();
 
 //  استقبال طلب على المسار / ي
-router.route("/").get(protect, getPosts)//.post(createPost);
+router.route("/").get(protect, getPosts).post(createPost);
 
 // DELETE api.lana.com/api/v1/posts/38959jekof23982r2
 // router.route("/:postId").get(getOnePost).patch(updatePost).delete(deletePost);
