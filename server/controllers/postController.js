69aafab811ab9e184b8da84e0330f453ff743efc
Hammam-Swaid <hmam.swid@gmail.com>
2024-02-22 20:24:05 +0200

    adding some filters to the text content

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,6 +1,12 @@
 const Post = require("../models/postModel");
 const catchAsync = require("../utils/catchAsync");
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const AppError = require("../utils/AppError");
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmProbability,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
 // const dotenv = require()
 
 exports.getPosts = async (req, res, next) => {
@@ -20,21 +26,40 @@ exports.getPosts = async (req, res, next) => {
   }
 };
 
-exports.createPost = catchAsync(async (req, res, next) => {
+exports.scanPost = catchAsync(async (req, res, next) => {
   const { content, photos } = req.body;
-  if (content) {
-    const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-    const textModel = genAI.getGenerativeModel({ model: "gemini-pro" });
+  if (!content) {
+    return next(new AppError("يجب أن يحتوي المنشور على محتوى نصي", 400));
+  }
+  const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+  const textModel = genAI.getGenerativeModel({ model: "gemini-pro" });
+  textModel.safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE
+    }
+  ];
+  const prompt = `what are the categories for this content: '${content}' : in the response separate between the categories by comma`;
+  const result = await textModel.generateContent(prompt);
+  const response = await result.response;
+  let text = response.text();
+  text = text.split(",");
+  if (photos) {
     const visionModel = genAI.getGenerativeModel({
       model: "gemini-pro-vision",
     });
-    const prompt = `what are the categories for this content: '${content}'`;
-    const result = await textModel.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
-    res.status(201).json({
-      text,
-    });
+  }
+  res.status(201).json({
+    text,
+  });
+});
+
+exports.createPost = catchAsync(async (req, res, next) => {
+  const { content, photos } = req.body;
+  if (content) {
   }
 });
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,12 +1,16 @@
 const express = require("express");
-const { getPosts, createPost } = require("../controllers/postController");
+const {
+  getPosts,
+  createPost,
+  scanPost,
+} = require("../controllers/postController");
 const { protect } = require("../controllers/authController");
 
 // إنشاء موجه فرعي خاص بالمناشير
 const router = express.Router();
 
 //  استقبال طلب على المسار / ي
-router.route("/").get(protect, getPosts).post(createPost);
+router.route("/").get(protect, getPosts).post(scanPost, createPost);
 
 // DELETE api.lana.com/api/v1/posts/38959jekof23982r2
 // router.route("/:postId").get(getOnePost).patch(updatePost).delete(deletePost);
