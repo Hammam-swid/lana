71174200d41a1593e0cbad201091dd6620a962d0
Hammam-Swaid <hmam.swid@gmail.com>
2024-03-10 07:01:16 +0200

    some design tests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 function App() {
   return (
     <>
-      <h1 className="text-8xl font-bold h-screen flex items-center justify-center">
+      <h1 className="text-8xl font-bold  flex items-center justify-center">
         السلام عليكم
       </h1>
+      <svg
+        className="animate-spin bg-slate-800 w-10 h-10 mr-3 rounded-full ring"
+        viewBox="0 0 24 24"
+      ></svg>
     </>
   );
 }
