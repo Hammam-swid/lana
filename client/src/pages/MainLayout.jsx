8ca6e47fd56cf5bae03266d346552bfcac4ae473
Hammam Swaid <hmam.swid@gmail.com>
2024-03-14 08:27:33 +0200

    adding web socket to the client and it is not complete

--- client/src/pages/MainLayout.jsx
+++ client/src/pages/MainLayout.jsx
@@ -1,14 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import io from "socket.io-client";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 function MainLayout() {
-  const socket = io("http://localhost:3000");
-  socket.on("connect", () => {
-    // console.log(socket.id);
-    socket.on('welcome', (message) => {
-      console.log(message)
-    })
+  const user = useSelector((state) => state.user);
+  const socket = io("http://localhost:3000", {
+    auth: { username: user.username },
   });
+  useEffect(() => {
+    socket.on("connect", () => {
+      socket.on("notification", (notification) => {
+        console.log(notification);
+      });
+    });
+  }, [socket]);
   return (
     <>
       <Header />
